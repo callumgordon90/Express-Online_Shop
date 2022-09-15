b13a3dfebc080cf7955d418b1f56c74444877bd0
callum <callumjosephiaingordon@gmail.com>
2022-09-15 18:00:25 +0200

    created products.router.js

--- index.js
+++ index.js
@@ -11,33 +11,7 @@ app.get('/nueva-ruta', (req, res) => {
   res.send('Hola, soy una nueva ruta');
 });
 
-//This endpoint is broken and I dont know why
-app.get('/products', (req, res) => {
-  const products = [];
-  const { size } = req.query;
-  const limit = size || 10;
-  for (let index = 0; index < limit; index++) {
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      image: faker.image.imageUrl(),
-    });
-  }
-  res.json(products);
-});
 
-app.get('/products/filter', (req, res) => {
-  res.send('Yo soy un filter');
-});
-
-app.get('/products/:id', (req, res) => {
-  const {id} = req.params;
-  res.json({
-    id,
-    name: 'Product 2',
-    price: 2000,
-  });
-});
 
 app.get('/users', (req, res) => {
   const {limit, offset} = req.query;
