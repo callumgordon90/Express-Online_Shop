306eafe480e6a451d72eba5467e064f34f3239b8
callum <callumjosephiaingordon@gmail.com>
2022-09-15 16:38:23 +0200

    feat: Remove faker

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+//const faker = require ('faker');
 const app = express();
 const port = 3000;
 
@@ -10,18 +11,17 @@ app.get('/nueva-ruta', (req, res) => {
   res.send('Hola, soy una nueva ruta');
 });
 
-app.get('/productos', (req, res) => {
-  res.json([
-    {
-      name: 'Product 1',
-      price: 1000,
-    },
-    {
-      name: 'Product 2',
-      price: 2000,
-    },
-  ]);
-});
+/*app.get('/products', (req, res) => {
+  const products = [];
+  for (let index = 0; index < 100; index++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.imageUrl(),
+    });
+  }
+  res.json(products);
+});*/
 
 app.get('/products/:id', (req, res) => {
   const {id} = req.params;
