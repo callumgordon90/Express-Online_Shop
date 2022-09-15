0298408c6fcde2a7e7a61ddfbc53364addbc4791
callum <callumjosephiaingordon@gmail.com>
2022-09-15 13:50:18 +0200

    added multiple routers with category id and product id

--- index.js
+++ index.js
@@ -4,20 +4,43 @@ const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hola mi server en express');
-})
+});
 
 app.get('/nueva-ruta', (req, res) => {
   res.send('Hola, soy una nueva ruta');
-})
+});
 
 app.get('/productos', (req, res) => {
-  res.json({
-    name: 'Product 1',
-    price:1000
+  res.json([
+    {
+      name: 'Product 1',
+      price: 1000,
+    },
+    {
+      name: 'Product 2',
+      price: 2000,
+    },
+  ]);
+});
 
-  })
+app.get('/products/:id', (req, res) => {
+  const {id} = req.params;
+  res.json({
+    id,
+    name: 'Product 2',
+    price: 2000,
+  });
+});
+
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const {categoryId, productId} = req.params;
+  res.json({
+    categoryId,
+    productId,
+  });
 })
 
 app.listen(port, () => {
-  console.log('Mi port ' + port );
+  console.log('Mi port ' + port);
 });
