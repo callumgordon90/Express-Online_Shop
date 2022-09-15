b1c2ba2fccfa1073c6d3e2e9058c1a694b8bb98d
callum <callumjosephiaingordon@gmail.com>
2022-09-15 13:09:05 +0200

    created two new routers in index.js

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ app.get('/', (req, res) => {
   res.send('Hola mi server en express');
 })
 
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Hola, soy una nueva ruta');
+})
+
+app.get('/productos', (req, res) => {
+  res.json({
+    name: 'Product 1',
+    price:1000
+
+  })
+})
+
 app.listen(port, () => {
   console.log('Mi port ' + port );
 });
