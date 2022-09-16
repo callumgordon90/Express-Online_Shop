31d25377e1c9e89d45590a26db573abc8cb36698
callum <callumjosephiaingordon@gmail.com>
2022-09-16 11:26:26 +0200

    updated the patch function and added a delete function in the products.router.js file

--- routes/products.router.js
+++ routes/products.router.js
@@ -45,13 +45,23 @@ router.post('/', (req, res) => {
 
 
 router.patch('/:id',(req, res) => {
+  const{ id } = req.params;
   const body = req.body;
   res.json({
-    message: 'created',
-    data: body
+    message: 'updated',
+    data: body,
+    id,
   });
 });
 
+
+router.delete('/:id',(req, res) => {
+  const{ id } = req.params;
+  res.json({
+    message: 'deleted',
+    id,
+  });
+});
 
 
 
